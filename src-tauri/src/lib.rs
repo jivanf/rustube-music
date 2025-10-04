01cdb962fa45a73ff4237484327fb7e10cc5eb45
Jivan Flores <jivan.flores.108@gmail.com>
2025-10-04 15:40:54 -0700

    build(deps): Install store Tauri plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,6 +6,7 @@ use commands::sign_in;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let mut builder = tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![sign_in]);
