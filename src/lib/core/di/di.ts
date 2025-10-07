e45469d8f81d43a40509ddaadf79f7f3456a49f3
Jivan Flores <jivan.flores.108@gmail.com>
2025-10-07 01:04:41 -0700

    refactor(di): Add factory provider

--- src/lib/core/di/di.ts
+++ src/lib/core/di/di.ts
@@ -1,5 +1,6 @@
 import { container } from '$lib/core/di/container';
-import { isClassProvider, type Provider } from '$lib/core/di/di.types';
+import { isClassProvider, isValueProvider, type Provider } from '$lib/core/di/di.types';
+import type { InjectionToken } from '$lib/core/di/injection-token';
 import type { Type } from '$lib/utils/types';
 import { isAsyncInitializable } from './init';
 
@@ -8,12 +9,15 @@ export async function provide(providers: Provider<unknown>[]): Promise<void> {
         let provide;
         let value;
 
-        if (isClassProvider(provider)) {
+        if (isValueProvider(provider)) {
+            provide = provider.provide;
+            value = provider.useValue;
+        } else if (isClassProvider(provider)) {
             provide = provider;
             value = new provider();
         } else {
             provide = provider.provide;
-            value = provider.useValue;
+            value = provider.useFactory();
         }
 
         if (isAsyncInitializable(value)) {
--- src/lib/core/di/di.types.ts
+++ src/lib/core/di/di.types.ts
@@ -1,12 +1,6 @@
 import type { InjectionToken } from '$lib/core/di/injection-token';
 import type { Type } from '$lib/utils/types';
 
-export type ClassProvider<TProvider> = Type<TProvider>;
-
-export function isClassProvider<TProvider>(provider: Provider<TProvider>): provider is ClassProvider<TProvider> {
-    return typeof provider === 'function';
-}
-
 export type ValueProvider<TProvider> = {
     provide: Type<TProvider> | InjectionToken<TProvider>;
     useValue: TProvider;
@@ -16,4 +10,15 @@ export function isValueProvider<TProvider>(provider: Provider<TProvider>): provi
     return typeof provider === 'object' && 'provide' in provider && 'useValue' in provider;
 }
 
-export type Provider<TProvider> = ClassProvider<TProvider> | ValueProvider<TProvider>;
+export type ClassProvider<TProvider> = Type<TProvider>;
+
+export function isClassProvider<TProvider>(provider: Provider<TProvider>): provider is ClassProvider<TProvider> {
+    return typeof provider === 'function';
+}
+
+export type FactoryProvider<TProvider> = {
+    provide: Type<TProvider> | InjectionToken<TProvider>;
+    useFactory: () => TProvider;
+};
+
+export type Provider<TProvider> = ValueProvider<TProvider> | ClassProvider<TProvider> | FactoryProvider<TProvider>;
