c975fca2930e64c06d247c2e6d1d332aaae7f526
Jivan Flores <jivan.flores.108@gmail.com>
2025-10-07 01:12:30 -0700

    refactor(utils): Add `nullish()`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,7 @@ export type WithoutChild<T> = T extends { child?: any } ? Omit<T, "child"> : T;
 export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, "children"> : T;
 export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
 export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
+
+export function nullish(value: unknown): value is null | undefined {
+	return value === null || value === undefined;
+}
