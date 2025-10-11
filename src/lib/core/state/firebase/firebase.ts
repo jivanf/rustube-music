041a8bae7eec820ade7c64f8dae31c8575584b13
Jivan Flores <jivan.flores.108@gmail.com>
2025-10-11 16:30:18 -0700

    chore(firebase): Remove app ID

--- src/lib/core/state/firebase/firebase.ts
+++ src/lib/core/state/firebase/firebase.ts
@@ -1,6 +1,6 @@
 import { type FirebaseApp, initializeApp } from 'firebase/app';
 import { type Auth, getAuth } from 'firebase/auth';
-import { PUBLIC_FIREBASE_API_KEY, PUBLIC_FIREBASE_APP_ID, PUBLIC_FIREBASE_PROJECT_ID } from '$env/static/public';
+import { PUBLIC_FIREBASE_API_KEY, PUBLIC_FIREBASE_PROJECT_ID } from '$env/static/public';
 
 export class FirebaseState {
     private readonly app: FirebaseApp;
@@ -10,7 +10,6 @@ export class FirebaseState {
     constructor() {
         this.app = initializeApp({
             projectId: PUBLIC_FIREBASE_PROJECT_ID,
-            appId: PUBLIC_FIREBASE_APP_ID,
             apiKey: PUBLIC_FIREBASE_API_KEY,
         });
 
