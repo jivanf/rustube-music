f6cb522501865fcf867ae60b1f90c2850e29487f
Jivan Flores <jivan.flores.108@gmail.com>
2025-10-12 18:23:32 -0700

    feat(home): Add home page

---
+++ src/routes/(protected)/home/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { parseFullName } from 'parse-full-name';
+    import { inject } from '$lib/core/di';
+    import { UserState } from '$lib/core/state/user';
+
+    const user = inject(UserState).user;
+
+    const firstName = $derived(parseFullName(user.displayName, 'first'))
+</script>
+
+<h1>Welcome {firstName}!</h1>
